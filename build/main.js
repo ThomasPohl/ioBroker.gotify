a57e3ab01c054512ffe7135ec4d74b3c7834b07d
Thomas Pohl <thomas.pohl@exxeta.com>
2023-05-24 16:27:27 +0200

    adapter-dev

--- build/main.js
+++ build/main.js
@@ -1,109 +1,84 @@
 "use strict";
-/*
- * Created with @iobroker/create-adapter v1.32.0
- */
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils = __importStar(require("@iobroker/adapter-core"));
-const axios_1 = __importDefault(require("axios"));
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var utils = __toESM(require("@iobroker/adapter-core"));
+var import_axios = __toESM(require("axios"));
 class Gotify extends utils.Adapter {
-    constructor(options = {}) {
-        super({
-            ...options,
-            name: "gotify",
-        });
-        this.on("ready", this.onReady.bind(this));
-        this.on("message", this.onMessage.bind(this));
-        this.on("unload", this.onUnload.bind(this));
+  constructor(options = {}) {
+    super({
+      ...options,
+      name: "gotify"
+    });
+    this.on("ready", this.onReady.bind(this));
+    this.on("message", this.onMessage.bind(this));
+    this.on("unload", this.onUnload.bind(this));
+  }
+  async onReady() {
+    this.log.debug("config url: " + this.config.url);
+    this.log.debug("config token: " + this.config.token);
+    if (!this.supportsFeature || !this.supportsFeature("ADAPTER_AUTO_DECRYPT_NATIVE")) {
+      this.config.token = this.decrypt(this.config.token);
     }
-    /**
-     * Is called when databases are connected and adapter received configuration.
-     */
-    async onReady() {
-        // Initialize your adapter here
-        // The adapters config (in the instance object everything under the attribute "native") is accessible via
-        // this.config:
-        this.log.debug("config url: " + this.config.url);
-        this.log.debug("config token: " + this.config.token);
-        if (!this.supportsFeature || !this.supportsFeature("ADAPTER_AUTO_DECRYPT_NATIVE")) {
-            this.config.token = this.decrypt(this.config.token);
-        }
-        if (!this.config.url || !this.config.token) {
-            this.log.error("Cannot send notification while not configured");
-        }
+    if (!this.config.url || !this.config.token) {
+      this.log.error("Cannot send notification while not configured");
     }
-    /**
-     * Is called when adapter shuts down - callback has to be called under any circumstances!
-     */
-    onUnload(callback) {
-        try {
-            callback();
-        }
-        catch (e) {
-            callback();
-        }
+  }
+  onUnload(callback) {
+    try {
+      callback();
+    } catch (e) {
+      callback();
     }
-    onMessage(obj) {
-        if (typeof obj === "object" && obj.message) {
-            if (obj.command === "send") {
-                this.sendMessage(obj.message);
-                // Send response in callback if required
-                if (obj.callback)
-                    this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-            }
-        }
+  }
+  onMessage(obj) {
+    if (typeof obj === "object" && obj.message) {
+      if (obj.command === "send") {
+        this.sendMessage(obj.message);
+        if (obj.callback)
+          this.sendTo(obj.from, obj.command, "Message received", obj.callback);
+      }
     }
-    sendMessage(message) {
-        if (this.config.url && this.config.token) {
-            axios_1.default
-                .post(this.config.url + "/message?token=" + this.config.token, {
-                title: message.title,
-                message: message.message,
-                priority: message.priority,
-                extras: {
-                    "client::display": {
-                        contentType: message.contentType,
-                    },
-                },
-            })
-                .then(() => {
-                this.log.debug("Successfully sent message to gotify");
-            })
-                .catch((error) => {
-                this.log.error("Error while sending message to gotify:" + JSON.stringify(error));
-            });
+  }
+  sendMessage(message) {
+    if (this.config.url && this.config.token) {
+      import_axios.default.post(this.config.url + "/message?token=" + this.config.token, {
+        title: message.title,
+        message: message.message,
+        priority: message.priority,
+        extras: {
+          "client::display": {
+            contentType: message.contentType
+          }
         }
-        else {
-            this.log.error("Cannot send notification while not configured");
-        }
+      }).then(() => {
+        this.log.debug("Successfully sent message to gotify");
+      }).catch((error) => {
+        this.log.error("Error while sending message to gotify:" + JSON.stringify(error));
+      });
+    } else {
+      this.log.error("Cannot send notification while not configured");
     }
+  }
 }
 if (require.main !== module) {
-    // Export the constructor in compact mode
-    module.exports = (options) => new Gotify(options);
+  module.exports = (options) => new Gotify(options);
+} else {
+  (() => new Gotify())();
 }
-else {
-    // otherwise start the instance directly
-    (() => new Gotify())();
-}
+//# sourceMappingURL=main.js.map
