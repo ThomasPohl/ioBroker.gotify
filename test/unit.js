17f8bdf9ef608869f6a45f844f9763f227f39936
Thomas Pohl <thomas.pohl@exxeta.com>
2024-05-19 06:28:14 +0200

    Add placeholder for unit test

--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,15 @@
 const path = require("path");
 const { tests } = require("@iobroker/testing");
 
-// Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, ".."));
+tests.unit(path.join(__dirname, ".."), {
+	//     ~~~~~~~~~~~~~~~~~~~~~~~~~
+	// This should be the adapter's root directory
+
+	// Define your own tests inside defineAdditionalTests.
+	// If you need predefined objects etc. here, you need to take care of it yourself
+	defineAdditionalTests() {
+		it("works", () => {
+			// see below how these could look like
+		});
+	},
+});
